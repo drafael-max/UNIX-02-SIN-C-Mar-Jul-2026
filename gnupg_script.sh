014b4c9f7c15986f108c042ea3e350e431ec48e2
drafael-max <drafaelpatin@gmail.com>
2026-03-23 15:46:28 +0000

    nuevo cambio

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,19 +1,24 @@
 #This command lists the private key
-
 gpg --list-secret-keys --keyid-format=long
 
 #Command to back up my private key
-
 gpg --armor --export-secret-keys
 678FEA3F7A530BBB
 
-#Ham
-
+#Hash
 678FEA3F7A530BBB
 
 #Export my key
-
 gpg --armor --export drafaelpatin@gmail.com > key_friend
 
 #Import my friend 
 key gpg --import llave_amigo
+
+#List the keys
+gpg --list-keys
+
+#Send a message
+echo "hola martin jaja" > doc_no_cifrado.txt
+
+#See the message 
+cat doc_no_cifrado.txt
