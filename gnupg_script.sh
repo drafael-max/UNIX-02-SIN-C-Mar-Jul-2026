acf810cee5bd11037e24a88e32d4bc77df639cb0
drafael-max <drafaelpatin@gmail.com>
2026-03-23 14:57:08 +0000

    mi primer commit

---
+++ gnupg_script.sh
@@ -0,0 +1,8 @@
+#Este comando enlista clave privada
+gpg --list-secret-keys --keyid-format=long
+
+#Comando para el respaldo de mi clave privadaa
+gpg --armor --export-secret-keys 678FEA3F7A530BBB
+#Hash
+678FEA3F7A530BBB
+
