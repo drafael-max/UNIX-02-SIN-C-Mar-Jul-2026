58bec7a56e5303aecff07eee055b0144f78304d9
drafael-max <drafaelpatin@gmail.com>
2026-03-30 15:09:19 +0000

    Run multiple commands in the terminal

---
+++ claselunes30script.sh
@@ -0,0 +1,11 @@
+#The package list is updated
+sudo apt update
+
+#Update installed programs
+sudo apt upgrade
+
+#The part is installed
+sudo apt install parted 
+
+#Install the NumPy library in Python
+pip install numpy
