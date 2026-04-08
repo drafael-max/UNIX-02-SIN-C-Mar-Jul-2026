56a5fc8c0b7a7684044d7897fdd9a42cfeba8b91
drafael-max <drafaelpatin@gmail.com>
2026-04-08 21:19:26 +0000

    command execution in classes

--- script_boot.sh
+++ script_boot.sh
@@ -1,5 +1,12 @@
-# Ruta relativa
+#Relative route
 cd calculadora
 
-# Ruta absoluta
+#Absolute route
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadoraa
+
+#l=list a=all i=inode 
+ls lai
+
+#They are the same options but separate
+ls -l -a -i
+
