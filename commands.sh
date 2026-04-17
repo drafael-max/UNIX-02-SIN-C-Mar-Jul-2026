56a56f13d7d87f990374e248b0a3042833c778d3
drafael-max <drafaelpatin@gmail.com>
2026-04-17 14:45:11 +0000

    We executed man and help commands and also looked for the depth

--- commands.sh
+++ commands.sh
@@ -19,3 +19,18 @@ mkdir -- -rf
 
 #Lists the files and folders in the current directory.
 ls 
+
+#Quick summary, fits on one screen.
+ls --help
+
+#Displays the complete manual for the ls command.
+man ls 
+
+#You navigate with arrows, you get out with q
+#/all search forward for "all"
+#n go to next result
+#N go to back to previous result 
+
+# Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to
+#fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
+--depth <depth>
