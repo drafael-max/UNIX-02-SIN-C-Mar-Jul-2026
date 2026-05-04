d0f59eec77b6342596f833b062933c42cc929ca0
drafael-max <drafaelpatin@gmail.com>
2026-05-04 01:01:14 +0000

    Finish section 2: Command syntax, arguments and options

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,27 @@
+#Linux course
+
+#The command executed is ls.
+#Lists the files and folders in the current directory.
+#Note: Linux is case-sensitive, use lowercase 'ls'.
+ls
+
+#Instead of listing everything, it only shows you what's inside the "Documents" folder.
+ls Documents
+
+#Command to manage programs, but with the argument moo activates a hidden message (Easter Egg).
+aptitude moo
+
+#It displays files with detailed information.
+ls -l
+
+#Displays the files in reverse order.
+ls -r
+
+#It displays the files with detailed information and in reverse order.
+ls -lr
+
+#Verbose options with aptitude: Changes the Easter Egg response
+#More 'v's increase the level of detail (verbosity)
+aptitude -v moo
+aptitude -vv moo
+aptitude -vvv moo
