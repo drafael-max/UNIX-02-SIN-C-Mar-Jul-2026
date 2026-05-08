1be8e7c89ce81fe9c8492dd9ead5fec8cdc7e094
drafael-max <drafaelpatin@gmail.com>
2026-05-08 14:36:38 +0000

    Add user and system information commands (id, groups, passwd display and first 10 lines)

---
+++ security.zsh
@@ -0,0 +1,11 @@
+#Displays information about the current user: User ID, group, and groups to which they belong.
+id
+
+#Displays the groups to which the current user belongs.
+groups
+
+#Displays the contents of the /etc/passwd file (system user list).
+cat /etc/passwd
+
+#Displays the first 10 lines of the /etc/passwd file.
+cat /etc/passwd | head -10
