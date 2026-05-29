6559ef53b7d4a6cc62c089091fa2992cd1860be8
drafael-max <drafaelpatin@gmail.com>
2026-05-29 15:07:36 +0000

    #We created a file before newgrp and ran ls -la to see the detailed information of the file

--- security.zsh
+++ security.zsh
@@ -6,3 +6,11 @@ id -gn #only the main group name
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #The group is the user's primary group
+
+#View the current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
