fa5b28c3826fd6170b9c92320f905dd8d7d3bac3
drafael-max <drafaelpatin@gmail.com>
2026-05-29 15:38:53 +0000

    Developer group configuration and validation completed

--- security.zsh
+++ security.zsh
@@ -14,3 +14,11 @@ echo "Grupo actual: $(id -gn)"
 #Create a file before newgrp
 touch ~/antes_de_newgrp.txt
 ls -la ~/antes_de_newgrp.txt
+
+#Access to the developers group was achieved by creating the group and changing the user's active group with the newgrp command.
+#The necessary packages were also installed.
+
+##Create a file within the subsell
+touch ~/despues_de_newgrp.txt
+ls -la ~/despues_de_newgrp.txt
+##The group is now 'desarrolladores'
