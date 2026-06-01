fe092d9f044442e8d197e7e1f65a7aca304c9d97
drafael-max <drafaelpatin@gmail.com>
2026-06-01 15:45:24 +0000

    Command execution and script creation 3.sh

---
+++ blackhatbash3.sh
@@ -0,0 +1,9 @@
+book="black hat bash"
+echo "This book's name is ${book}"
+echo "This book's name is $book"
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+
+book="Black Hat Bash" 
+unset book 
+echo "${book}"
