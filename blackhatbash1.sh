ab727f0992d0d6fac5dbc783445a942da89a31b5
drafael-max <drafaelpatin@gmail.com>
2026-06-01 15:20:46 +0000

    loop

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,4 +1,4 @@
-bash --versionecho $SHELL
+bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
@@ -9,7 +9,9 @@ ps -ef
 df --human-readable
 #!/bin/bash
 #!/usr/bin/env bash
-bash -x blackhatbash1.sh
-bash -r blackhatbash1.sh
+#bash -x blackhatbash1.sh
+#bash -r blackhatbash1.sh
 #script.sh
 #bash script.sh
+#An infinite loop occurred because the script was running itself using the command `bash -x blackhatbash1.sh`. 
+#Each time the script reached that line, it started a new execution of the same file, repeating the process indefinitely until it was manually stopped.
