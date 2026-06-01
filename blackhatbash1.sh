6f1a0ee26371a948081d58c48bd53b3c26ddb41d
drafael-max <drafaelpatin@gmail.com>
2026-06-01 15:05:17 +0000

    command execution

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,15 @@
-bash --version
+bash --versionecho $SHELL
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -e -f
+ps -ef
+df --human-readable
+#!/bin/bash
+#!/usr/bin/env bash
+bash -x blackhatbash1.sh
+bash -r blackhatbash1.sh
+#script.sh
+#bash script.sh
