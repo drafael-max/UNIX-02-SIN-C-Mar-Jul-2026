d722e787bed8332df2b30eb54cf9690a45fe4af2
drafael-max <drafaelpatin@gmail.com>
2026-06-05 15:39:44 +0000

    Practice Bash file redirection and output stream management

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,21 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl falló."
+
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World" > output.txt
+cat outpu.txt
+echo "Goodbye!" >> output.txt
+cat outpu.txt
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+
