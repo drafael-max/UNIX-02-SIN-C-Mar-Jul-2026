83e732af97d06028e16bffdca9d1432879aa8a48
drafael-max <drafaelpatin@gmail.com>
2026-06-05 15:15:03 +0000

    Add script demonstrating background processes and file management

--- array.sh
+++ array.sh
@@ -3,11 +3,11 @@
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 
 # Prints all elements in the array
-echo "${IP_ADDRESSES[*]}"
-# Prints only the first element in the array
 set -x
-echo "${IP_ADDRESSES[0]}"
+echo "${IP_ADDRESSES[*]}"
 set +x
+# Prints only the first element in the array
+echo "${IP_ADDRESSES[0]}"
 
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 echo "${IP_ADDRESSES[*]}"
