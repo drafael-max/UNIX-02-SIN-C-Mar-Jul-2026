2402a7b1b97e5ff9c3c479c2019eac76f44a9773
drafael-max <drafaelpatin@gmail.com>
2026-06-08 15:25:09 +0000

    Iterate through and print each argument received by the script on a separate line.

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -9,3 +9,8 @@ ping -c 3 "${TARGET}"
 
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
+
+# Change "$@" to "$*" to observe behavior.
+for args in "$@"; do
+ echo "${args}"
+done
