a70f73afa3ae856b8e1a5884a82e2849557e996e
drafael-max <drafaelpatin@gmail.com>
2026-06-19 15:30:50 +0000

    Practice awk column extraction with CSV files

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -7,4 +7,19 @@ ps | grep TTY
 ps | grep tty
 ps | grep -i tty
 grep -v "35.237.4.214" log.txt
-grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+grep -o "35.237.4.214" log.txt
+awk '{print $1}' log.txt
+awk '{print $1,$2,$3}' log.txt
+awk '{print $1,$NF}' log.tx
+
+cat > test.csv << EOF
+nombre,edad
+Rafael,20
+Juan,25
+Maria,22
+EOF
+
+cat test.csv
+
+awk -F',' '{print $1}' test.csv
+awk -F',' '{print $2}' test.csv
